81ca6306b61e34c07af868a161620222d8aa293d
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-08-28 18:51:57 +0700

    completing sign in and sign out feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+import { auth } from './firebase/firebase.utils.js'
 import Homepage from "./pages/Homepage/Homepage.jsx";
 import ShopPage from "./pages/ShopPage/ShopPage.jsx";
 import './styles/App.css'
@@ -7,9 +9,19 @@ import {SignInPage} from "./pages/SignInPage/SignInPage.jsx";
 import {SignUpPage} from "./pages/SignUpPage/SignUpPage.jsx";
 
 function App() {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+
+            console.log(user)
+        })
+    }, [])
+
     return (
         <>
-            <Header/>
+            <Header currentUser={currentUser}/>
             <Switch>
                 <Route exact path="/" component={Homepage}/>
                 <Route exact path="/shop" component={ShopPage}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,32 @@
 import './Header.styles.scss';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '/src/assets/crown.svg';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '/src/assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils.js'
+import prompt from "react-router-dom/es/Prompt.js";
 
-const routeData = [
-    {
-        name: "SHOP",
-        url: "/shop"
-    },
-    {
-        name: "CONTACT",
-        url: "/shop"
-    },
-    {
-        name: "SIGN IN",
-        url: "/shop/signin"
-    }
-];
-
-export const Header = () => {
+export const Header = ({currentUser}) => {
     return (
         <div className="header">
             <Link className="logo-container" to="/">
                 <Logo className="logo"/>
             </Link>
             <div className="options">
+                <Link className="option" to="/shop">SHOP</Link>
+                <Link className="option" to="/contact">CONTACT</Link>
                 {
-                    routeData.map(({ name, url }) => (
-                        <Link className="option" to={url}>{name}</Link>
-                    ))
+                    currentUser === null ? (
+                        <Link className="option" to="/shop/signin">SIGN IN</Link>
+                    ) : (
+                        <Link className="option" to="/" onClick={async () => {
+                            await prompt("Apakah anda ingin logout ?")
+                            await auth.signOut()
+                        }
+                        }
+                        >SIGN OUT</Link>
+                    )
                 }
+
+
             </div>
         </div>
     )
